d891c28397a7b6b52d6586985c361ff903361fa4
Thomas Clark <thc29@144.115.248.10.in-addr.arpa.private.cam.ac.uk>
2016-12-19 18:03:02 +0000

    WIP Implementing unit tests for bins and profiles. Had to refactor Profile implementations into the header file, because the templating system doesn't work if they're in the .cpp file. Refactored everything toward using std::vector instead of double*.

--- test/unit/fit_profiles_test.cpp
+++ test/unit/fit_profiles_test.cpp
@@ -1,18 +1,138 @@
 /*
- * Fit_test.cpp
- *
- *  Created on: 29 Nov 2016
- *      Author: thc29
+ * To parameterise tests, check out minute 9 onward in https://www.youtube.com/watch?v=16FI1-d2P4E&t=20s
  */
 
 #include "gtest/gtest.h"
-#include "fit_profiles.h"
+#include "profile.h"
+//#include "fit_profiles.h"
+
 
 using namespace es;
 
-TEST(basic_check, test_eq) {
-    Fit my_fit;
-    int my_check_value = 0;
 
-    EXPECT_EQ(my_check_value, my_fit.Fitness());
+
+// Test fixture for the Bin class
+class BinTest : public ::testing::Test {
+
+protected:
+
+    virtual void SetUp() {
+        // Code here will be called immediately after the constructor (right
+        // before each test).
+        std::cout << std::endl << "Setting up BinTest()..." << std::endl;
+    }
+
+    virtual void TearDown() {
+        // Code here will be called immediately after each test (right
+        // before the destructor).
+        std::cout << "Tearing down BinTest()..." << std::endl;
+    }
+
+};
+
+TEST_F(BinTest, test_construct_bins){
+
+    // Construct 10 bins spaced equally 1 m apart from 1m above reference using the base constructor
+    std::vector<double> z;
+    for (int i=0; i<10; i++) z.push_back(double(i+1));
+    Bins b1 = Bins(z);
+
+    // Ensure that the printing operator does not error
+    std::cout << b1 << std::endl;
+
+    // Check value
+    EXPECT_EQ(z[9], double(10));
+
 }
+
+// Test fixture for the Profile class
+class ProfileTest : public ::testing::Test {
+
+protected:
+
+    virtual void SetUp() {
+        std::cout << std::endl << "Setting up BinTest()..." << std::endl;
+    }
+
+    virtual void TearDown() {
+        std::cout << "Tearing down BinTest()..." << std::endl;
+    }
+
+};
+
+
+TEST_F(ProfileTest, test_construct_double_profile){
+
+    // Construct profiles with 100 bins spaced equally 1 m apart from 1m above reference using the base constructor
+    std::vector<double> z;
+    for (int i=0; i<100; i++) z.push_back(double(i+1));
+    Bins bins = Bins(z);
+
+    Profile<double> p1(bins);
+    Profile<double> p2(bins, 10.1, 0.2, 0.1);
+
+    // Ensure that the printing operator does not error (before values are assigned)
+    std::cout << p1 << std::endl;
+
+    // Assign values (init as zeros, the value doesn't matter here)
+    std::vector<double> vals(100);
+    p1.setValues(vals);
+
+    // This should crap out due to incorrect number of bins
+    vals.push_back(0.0);
+    try {
+        p1.setValues(vals);
+        FAIL() << "Expected std::range_error";
+    }
+    catch(std::range_error const & err) {
+        EXPECT_EQ(err.what(),std::string("Length of the values vector does not equal the number of bins for this profile."));
+    }
+    catch(...) {
+        FAIL() << "Expected std::range_error";
+    }
+
+}
+
+// Test fixture for profile fitting
+class FitTest : public ::testing::Test {
+
+protected:
+
+    // TODO remove any or all of the following functions if bodies are empty
+
+    FitTest() {
+        // You can do set-up work for each test here.
+    }
+
+    virtual ~FitTest() {
+        // You can do clean-up work that doesn't throw exceptions here.
+    }
+
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
+
+    virtual void SetUp() {
+        // Code here will be called immediately after the constructor (right
+        // before each test).
+        std::cout << std::endl << "Setting up FitTest()..." << std::endl;
+    }
+
+    virtual void TearDown() {
+        // Code here will be called immediately after each test (right
+        // before the destructor).
+        std::cout << std::endl << "Tearing down FitTest()..." << std::endl;
+    }
+
+    // Objects declared here can be used by all tests
+    int a= 11;
+};
+
+
+//// Tests that the Fit::Bar() method does Abc.
+//TEST_F(FitTest, test_eq) {
+//    Fit my_fit;
+//    int my_check_value = a;
+//    std::cout << my_check_value << std::endl;
+//
+//    EXPECT_EQ(my_check_value, my_fit.Fitness());
+//}
