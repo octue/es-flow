b3733e54312d1dab42ece0c64c7d83437b288c5e
Thomas Clark <thc29@144.115.248.10.in-addr.arpa.private.cam.ac.uk>
2016-12-23 10:29:23 +0000

    WIP on file readers for LiDAR. Currently loading a .mat file of defined type.

--- source/readers.h
+++ source/readers.h
@@ -17,45 +17,63 @@
 #include "matio.h"
 #include "glog/logging.h"
 
+namespace es {
 
-/** Level of data validation applied to timeseries. Exception is thrown if timeseries checks fail.
- */
-enum timeseries_check_level {
-    PRESENT             =  0,    // Checks that data is present and of correct type. Extracts start and end timestamps.
-    INCREASING          =  1,    // As PRESENT + checks that the timestamp always increases.
-    MONOTONIC           =  2,    // As INCREASING + checks that timestamp uniformly increases, allows data skip (e.g. instrument turned off then back on later). Extracts sampling frequency.
-    STRICTLY_MONOTONIC  =  3,    // AS INCREASING + checks that timestamp uniformly increases, data skipping causes errors. Extracts sampling frequency.
-};
+    // Level of data validation applied to timeseries. Exception is thrown if timeseries checks fail
+    enum timeseries_check_level {
+        PRESENT             = 0,    // Checks that data is present and of correct type. Extracts start and end timestamps.
+        INCREASING          = 1,    // As PRESENT + checks that the timestamp always increases.
+        MONOTONIC           = 2,    // As INCREASING + checks that timestamp uniformly increases, allows data skip (e.g. instrument turned off then back on later). Extracts sampling frequency.
+        STRICTLY_MONOTONIC  = 3,    // AS INCREASING + checks that timestamp uniformly increases, data skipping causes errors. Extracts sampling frequency.
+    };
 
-class Reader {
-public:
-    Reader(const std::string &file);
+    class Reader {
+    public:
+        Reader(const std::string &file);
 
-    ~Reader();
+        ~Reader();
 
-    void checkTimeseries(enum timeseries_check_level);
+        void checkTimeseries(enum timeseries_check_level);
 
-protected:
-    std::string file;
-    mat_t * matfp;
-    std::string file_type;
+        void checkFileType();
 
-};
+        std::string logString() const;
 
-class LidarReader : public Reader {
-public:
-    LidarReader(const std::string &file);
+    protected:
+        std::string file;
+        mat_t *matfp;
+        std::string file_type;
 
-    ReadFull();
-    ReadWindow();
+    };
+
+    // Represent Reader classes in logs or ostream
+    ::std::ostream& operator<<(::std::ostream& os, const Reader& reader);
+
+    class LidarReader : public Reader {
+    public:
+        LidarReader(const std::string &file);
+
+        void read();
+
+        double getWindowDuration() const;
+
+        void setWindowDuration(double windowDuration);
+
+    protected:
+        double dt;
+        std::vector<double> t;
+        int windowSize;
+        double windowDuration;
+
+    public:
+        int getWindowSize() const;
+
+        void setWindowSize(int windowSize);
+
+    };
+
+} /* namespace es */
 
 
-protected:
-    double dt;
-    std::vector<double> t;
-    int windowSize;
-
-
-};
 
 #endif //ES_FLOW_READERS_H
