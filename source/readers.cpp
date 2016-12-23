2e266b0778910da52b7d29b5d63f033b6974c504
Thomas Clark <thc29@144.115.248.10.in-addr.arpa.private.cam.ac.uk>
2016-12-23 14:12:08 +0000

    Massively refactored the readers class to be templated on the data type. This will help us to avoid subclassing Reader() for every data type, whilst drawing out and making the data structure definitions really obvious.

--- source/readers.cpp
+++ source/readers.cpp
@@ -27,85 +27,17 @@
 
 namespace es {
 
-    Reader::Reader(const std::string &file) : file(file) {
+//    LidarReader::LidarReader(const std::string &file) : Reader(file) {
+//
+//        // Check the file type
+//        checkFileType();
+//        if (file_type != "lidar_basic") {
+//            std::string msg = "Data file of incorrect type: Expected 'lidar_basic', got: ";
+//            throw std::invalid_argument(msg + file_type);
+//        }
+//
+//    }
 
-        // Open the MAT file for reading and save the pointer
-        matfp = Mat_Open(file.c_str(), MAT_ACC_RDONLY);
-        if (matfp == NULL) {
-            std::string msg = "Error reading MAT file: ";
-            throw std::invalid_argument(msg + file);
-        }
 
-    }
-
-    Reader::~Reader() {
-
-        // Close the file on destruction of the Reader
-        Mat_Close(matfp);
-
-    }
-
-    void Reader::checkFileType(){
-
-        // Get the file type from the reserved 'type' variable in the mat file
-        matvar_t *type_var = Mat_VarRead(matfp, "type");
-        if (type_var == NULL) {
-            throw std::invalid_argument(
-                "Error reading mat file (most likely not an OAS standard file format - 'type' variable is missing)");
-        } else {
-            if (type_var->class_type != MAT_C_CHAR) {
-                throw std::invalid_argument("Error reading mat file ('type' variable must be a character array)");
-            }
-            // Mat_VarPrint(type_var, true);
-            file_type = std::string((const char*)type_var->data, type_var->dims[1]);
-        }
-
-    }
-
-    void Reader::checkTimeseries(enum timeseries_check_level) {
-
-        // Check the integrity of the timeseries, automatically determine dt and frequency where possible
-
-    }
-
-    std::string Reader::logString() const {
-        return std::string("Object Reader for type ") + file_type + std::string(", attached to file file ") + file;
-    }
-
-    ::std::ostream& operator<<(::std::ostream& os, const Reader& reader) {
-        // Represent in logs or ostream
-        return os << reader.logString();
-    }
-
-    LidarReader::LidarReader(const std::string &file) : Reader(file) {
-
-        // Check the file type
-        checkFileType();
-        if (file_type != "lidar_basic") {
-            std::string msg = "Data file of incorrect type: Expected 'lidar_basic', got: ";
-            throw std::invalid_argument(msg + file_type);
-        }
-
-    }
-
-    double LidarReader::getWindowDuration() const {
-        return windowDuration;
-    }
-
-    void LidarReader::setWindowDuration(double windowDuration) {
-        LidarReader::windowDuration = windowDuration;
-    }
-
-    int LidarReader::getWindowSize() const {
-        return windowSize;
-    }
-
-    void LidarReader::setWindowSize(int windowSize) {
-        LidarReader::windowSize = windowSize;
-    }
-
-    void LidarReader::read() {
-
-    }
 
 } /* namespace es */
