bbe97d28e90341b6a21d38f6de6062dac303a9bd
Tom Clark <>
2019-01-21 15:41:10 +0000

    IMP Reynolds Stress 13 code along with a unit test, verified against MATLAB. still contains teh bug of issue #39

--- test/unit/adem_analysis_test.cpp
+++ test/unit/adem_analysis_test.cpp
@@ -17,11 +17,14 @@
  *
  */
 
-#include "gtest/gtest.h"
-#include "adem/adem.h"
 #include <Eigen/Dense>
 #include <Eigen/Core>
 #include <unsupported/Eigen/AutoDiff>
+
+#include "gtest/gtest.h"
+
+#include "adem/adem.h"
+#include "relations/stress.h"
 
 using namespace es;
 using namespace Eigen;
@@ -69,14 +72,14 @@ TEST_F(AdemAnalysisTest, test_analysis) {
     signature_b = signature_b + signature_bx;
 
     // Basic test parameters
-    double pi_coles = 0.42;
-    double kappa = 0.41;
+    double beta = 0.0;
     double delta_c = 1000.0;
-    double u_inf = 20.0;
+    double kappa = 0.41;
+    double pi_coles = 0.42;
     double shear_ratio = 23.6;
+    double u_inf = 20.0;
     double u_tau = u_inf / shear_ratio;
     double z_0 = 0.0;
-    double beta = 0.0;
     double zeta = 0.0;
 
     // Run ADEM for these parameters to get full spectra and stuff
@@ -85,8 +88,6 @@ TEST_F(AdemAnalysisTest, test_analysis) {
     // Load in the verification data (computed with MATLAB)
 //    AdemData check_data = AdemData();
 //    check_data.load(data_path + std::string("/adem_data_check.mat"));
-
-
 
     // Print useful diagnostics values
 //    std::cout << "speed = ["     << speed.transpose()     << "];" << std::endl;
@@ -101,4 +102,46 @@ TEST_F(AdemAnalysisTest, test_analysis) {
     //    std::cout << "z_0 = " << z_0 << ";" << std::endl;
     //    std::cout << "z = [" << z << "];" << std::endl;
 }
+
+TEST_F(AdemAnalysisTest, test_get_reynolds_stress_13) {
+
+    // Results obtained and validated against MATLAB:
+    /* eta = [0.001, 0.1, 0.3, 0.6, 1.0];
+     * pi_coles = 0.42;
+     * zeta = 0;
+     * beta = 0;
+     * shear_ratio = 23.6;
+     * [r13_a, r13_b] = getReynoldsStress13(eta, pi_coles, shear_ratio, zeta, beta)
+     */
+    Eigen::ArrayXd eta;
+    Eigen::ArrayXd r13_a;
+    Eigen::ArrayXd r13_b;
+    Eigen::ArrayXd r13_a_correct;
+    Eigen::ArrayXd r13_b_correct;
+    double beta = 0.0;
+    double kappa = 0.41;
+    double pi_coles = 0.42;
+    double shear_ratio = 23.6;
+    double zeta = 0.0;
+
+    eta.setZero(5);
+    r13_a_correct.setZero(5);
+    r13_b_correct.setZero(5);
+
+    eta << 0.001, 0.1, 0.3, 0.6, 1.0;
+    r13_a_correct << -0.991958214632306, -0.663877109187046, -0.323638466476833, -0.072136229566514, 0;
+    r13_b_correct << -0.015941694827107, -0.292636189577533, -0.515677923020367, -0.430112433486726, 0.000000000000000;
+
+    reynolds_stress_13(r13_a, r13_b, beta, eta, kappa, pi_coles, shear_ratio, zeta);
+
+    std::cout << "r13a expected: " << r13_a_correct << std::endl;
+    std::cout << "r13a returned: " << r13_a << std::endl;
+
+    std::cout << "r13b expected: " << r13_b_correct << std::endl;
+    std::cout << "r13b returned: " << r13_b << std::endl;
+
+    ASSERT_TRUE(r13_a.isApprox(r13_a_correct));
+    ASSERT_TRUE(r13_b.isApprox(r13_b_correct));
+
+    }
 
