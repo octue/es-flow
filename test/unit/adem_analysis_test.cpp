2ecb8bbb60a7956c82fc89226b2f1038369d1cab
Tom Clark <>
2019-01-22 13:36:14 +0000

    IMP Added filter and deconv functions with unit test to mimic those of MATLAB

--- test/unit/adem_analysis_test.cpp
+++ test/unit/adem_analysis_test.cpp
@@ -25,6 +25,7 @@
 
 #include "adem/adem.h"
 #include "relations/stress.h"
+#include "utilities/filter.h"
 
 using namespace es;
 using namespace Eigen;
@@ -82,12 +83,15 @@ TEST_F(AdemAnalysisTest, test_analysis) {
     double z_0 = 0.0;
     double zeta = 0.0;
 
+
     // Run ADEM for these parameters to get full spectra and stuff
     AdemData data = adem(beta, delta_c, kappa, pi_coles, shear_ratio, u_inf, zeta, signature_a, signature_b);
 
-    // Load in the verification data (computed with MATLAB)
+//    // Load in the verification data (computed with MATLAB)
 //    AdemData check_data = AdemData();
+//    std::cout << check_data;
 //    check_data.load(data_path + std::string("/adem_data_check.mat"));
+//    std::cout << check_data;
 
     // Print useful diagnostics values
 //    std::cout << "speed = ["     << speed.transpose()     << "];" << std::endl;
@@ -105,7 +109,7 @@ TEST_F(AdemAnalysisTest, test_analysis) {
 
 TEST_F(AdemAnalysisTest, test_get_reynolds_stress_13) {
 
-    // Results obtained and validated against MATLAB:
+    // Results obtained and validated against MATLAB implementation:
     /* eta = [0.001, 0.1, 0.3, 0.6, 1.0];
      * pi_coles = 0.42;
      * zeta = 0;
@@ -134,14 +138,50 @@ TEST_F(AdemAnalysisTest, test_get_reynolds_stress_13) {
 
     reynolds_stress_13(r13_a, r13_b, beta, eta, kappa, pi_coles, shear_ratio, zeta);
 
-    std::cout << "r13a expected: " << r13_a_correct << std::endl;
-    std::cout << "r13a returned: " << r13_a << std::endl;
+    std::cout << "r13a expected: " << r13_a_correct.transpose() << std::endl;
+    std::cout << "r13a returned: " << r13_a.transpose() << std::endl;
 
-    std::cout << "r13b expected: " << r13_b_correct << std::endl;
-    std::cout << "r13b returned: " << r13_b << std::endl;
+    std::cout << "r13b expected: " << r13_b_correct.transpose() << std::endl;
+    std::cout << "r13b returned: " << r13_b.transpose() << std::endl;
 
     ASSERT_TRUE(r13_a.isApprox(r13_a_correct));
     ASSERT_TRUE(r13_b.isApprox(r13_b_correct));
 
     }
+
+
+TEST_F(AdemAnalysisTest, test_filter_and_deconv) {
+
+    Eigen::ArrayXd a(5);
+    Eigen::ArrayXd b(6);
+    Eigen::ArrayXd x(10);
+    Eigen::ArrayXd y;
+    Eigen::ArrayXd y_correct(10);
+    Eigen::ArrayXd z;
+    Eigen::ArrayXd z_correct(2);
+
+    // Set up arbitrary filter inputs
+    a << 1,    0.4,  0.2,  0.5,  0.1;
+    b << 0.45, 0.34, 0.65, 0.32, 0.46, 0.98;
+    x << 0.814723686393179, 0.905791937075619, 0.126986816293506, 0.913375856139019, 0.632359246225410,
+         0.097540404999410, 0.278498218867048, 0.546881519204984, 0.957506835434298, 0.964888535199277;
+
+    // Correct outputs for filter and deconvolution determined with MATLAB
+    y_correct << 0.366625658876931, 0.537962161506937, 0.606173725715194, 0.770296239521390, 0.607280310491784,
+                 1.354464466281476, 0.698884413086931, 0.220025836201273, 1.049300675330637, 0.920311139464538;
+    z_correct << 0.450000000000000, 0.160000000000000;
+
+    filter(y, b, a, x);
+    deconv(z, b, a);
+
+    std::cout << "y expected: " << y_correct.transpose() << std::endl;
+    std::cout << "y returned: " << y.transpose() << std::endl;
+
+    std::cout << "z expected: " << z_correct.transpose() << std::endl;
+    std::cout << "z returned: " << z.transpose() << std::endl;
+
+    ASSERT_TRUE(y.isApprox(y_correct));
+    ASSERT_TRUE(z.isApprox(z_correct));
+
+}
 
