3b941d1cb24a296f7989b0c95f9380842d1160d3
Tom Clark <>
2019-03-12 11:53:35 +0000

    Added exceptions file, not currently used as there is a conflict between that and cpplot/exceptions.h that needs to be resolved by correct namespacing.

---
+++ source/exceptions.h
@@ -0,0 +1,37 @@
+/*
+ * exceptions.h Customised exceptions for appropriate and fine grained error handling
+ *
+ * Author:              Tom Clark  (thclark@github)
+ *
+ * Copyright (c) 2017-8 T Clark. All Rights Reserved.
+ *
+ */
+
+#ifndef ES_FLOW_EXCEPTIONS_H
+#define ES_FLOW_EXCEPTIONS_H
+
+
+#include <exception>
+
+
+namespace es {
+
+
+struct NotImplementedException : public std::exception {
+    std::string message = "Not yet implemented";
+    const char *what() const throw() {
+        return message.c_str();
+    }
+};
+
+struct InvalidEddyTypeException : public std::exception {
+    std::string message = "Unknown eddy type. Eddy type string must be one of 'A', 'B1', 'B2', 'B3' or 'B4'.";
+    const char *what() const throw() {
+        return message.c_str();
+    }
+
+};
+
+}
+
+#endif // ES_FLOW_EXCEPTIONS_H
