ae8e1e7686ef428dcb6cc2e684a205c2b1a61c5f
Tom Clark <>
2019-03-12 11:55:07 +0000

    Added header stubs to contain the library public API and teh smearing functions/classes

---
+++ source/flow.h
@@ -0,0 +1,16 @@
+/*
+ * flow.h Sketching out publiic api for the flow library
+ * 
+ * Author:              Tom Clark  (thclark @ github)
+ *
+ * Copyright (c) 2019 Octue Ltd. All Rights Reserved.
+ *
+ */
+
+#ifndef ES_FLOW_FLOW_H
+#define ES_FLOW_FLOW_H
+
+/*
+
+
+#endif //ES_FLOW_FLOW_H
